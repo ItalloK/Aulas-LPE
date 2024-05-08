7239476ac0cba50e14e1ffb7f0a80abde834d8f9
Italo <itallo016@gmail.com>
2024-05-08 09:21:29 -0300

    Questao 3

--- Exercicios Array/Questao3.c
+++ Exercicios Array/Questao3.c
@@ -4,17 +4,17 @@
 se encontram o maior e o menor valor.*/
 
 int main(){
-    int i, menor_pos, maior_pos; 
+    int i, menor_pos = 0, maior_pos = 0; 
     int Vetor[10];
     for(i = 0; i < 10; i++){
         printf("Digite o %d valor: \n", i);
         scanf("%d", &Vetor[i]);
-        if(Vetor[i] < Vetor[i-1]){
-            menor_pos = i;
-        }
-        if(Vetor[i] > Vetor[i-1]){
+        if(Vetor[i] > Vetor[maior_pos]){
             maior_pos = i;
         }
+        if(Vetor[i] < Vetor[menor_pos]){
+            menor_pos = i;
+        } 
     }
     for(i = 0; i<10; i++){
         printf("pos: %d, valor: %d\n", i,  Vetor[i]);
