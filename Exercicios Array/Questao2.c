05b08357309a7f40266b86f4d23c34c64a3412e3
Italo <itallo016@gmail.com>
2024-05-07 22:46:18 -0300

    Questao 2

--- Exercicios Array/Questao2.c
+++ Exercicios Array/Questao2.c
@@ -5,6 +5,23 @@ o quadrado de cada um destes valores desse primeiro vetor e os armazene em outro
 inversas. O conteúdo dos dois vetores deve ser exibido.*/
 
 int main(){
-    
+    float Vetor[5], Vetor2[5];
+    int i = 0;
+    for(i = 0; i < 5; i++){
+        printf("Digite o %d numero real: \n", i+1);
+        scanf("%f", &Vetor[i]);
+    }
+    for(i = 0; i < 5; i++){
+        Vetor[i] = (Vetor[i] * Vetor[i]);
+        printf("VETOR 1: Posicao %d valor: %f\n", i, Vetor[i]);
+    }
+    int b = 4;
+    for(i = 0; i < 5; i++){
+        Vetor2[i] = Vetor[b];
+        b--;
+    }
+    for(i = 0; i < 5; i++){
+        printf("VETOR 2: Posicao %d valor: %f\n", i, Vetor2[i]);
+    }
     return 0;
 }
--- Exercicios Array/Questao3.c
+++ Exercicios Array/Questao3.c
@@ -4,6 +4,21 @@
 se encontram o maior e o menor valor.*/
 
 int main(){
-    
+    int i, menor_pos, maior_pos; 
+    int Vetor[10];
+    for(i = 0; i < 10; i++){
+        printf("Digite o %d valor: \n", i);
+        scanf("%d", &Vetor[i]);
+        if(Vetor[i] < Vetor[i-1]){
+            menor_pos = i;
+        }
+        if(Vetor[i] > Vetor[i-1]){
+            maior_pos = i;
+        }
+    }
+    for(i = 0; i<10; i++){
+        printf("pos: %d, valor: %d\n", i,  Vetor[i]);
+    }
+    printf("Posicao do menor: %d e Posicao do Maior: %d\n", menor_pos, maior_pos);
     return 0;
 }
